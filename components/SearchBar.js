9dd926bfec1d9af0b42533f9bf9a10c6c30374b1
magdi <m49dii@gmail.com>
2023-02-15 11:47:10 +0200

    Adding the search functionality

---
+++ components/SearchBar.js
@@ -0,0 +1,18 @@
+import { TextInput, View } from "react-native";
+
+export default function SearchBar ({
+    search,
+    setSearch,
+}) {
+    return (
+        <View className="mb-3 flex-row items-center">
+            <TextInput 
+                inputMode="search"
+                value={search}
+                placeholder="search"
+                onChange={(phrase) => setSearch(phrase.nativeEvent.text)}
+                className="block w-11/12 px-3 py-2 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-black/20 focus:border-black/40"
+            />
+        </View>
+    )
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SearchBar from '../components/SearchBar';
 import ProductList from '../components/ProductList';
 
 export default function Home() {
+  const [search, setSearch] = useState("");
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-gray-100">
-      <ProductList />
+    <SafeAreaView className="items-center justify-center bg-gray-100">
+      <SearchBar
+        search={search}
+        setSearch={setSearch}
+      />
+      <ProductList search={search} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
